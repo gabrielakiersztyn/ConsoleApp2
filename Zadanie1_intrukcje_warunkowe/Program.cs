4ceefe1717824037e6149bcc7466412d3dcfdb66
gabrielakiersztyn <g.kiersztyn.031@studms.ug.edu.pl>
2025-03-07 14:41:13 +0100

    koniec

---
+++ Zadanie1_intrukcje_warunkowe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1_intrukcje_warunkowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to
+            //liczba parzysta, czy też nieparzysta.
+            int a;
+            Console.Write("Podaj liczbę całkowitą: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+                Console.Write("Liczba {0} jest parzysta", a);
+            else
+
+
+        }
+    }
+}
---
+++ Zadanie1_intrukcje_warunkowe/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zadanie1_intrukcje_warunkowe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadanie1_intrukcje_warunkowe")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("4fa8da8d-fcae-4516-ac16-4c820096913b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
