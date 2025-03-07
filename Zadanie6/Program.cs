4bd2cd6f118157fdfd8dea385461a6edc7b70f0f
gabrielakiersztyn <g.kiersztyn.031@studms.ug.edu.pl>
2025-03-07 14:28:26 +0100

    koniec

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby i wypisujący na ekranie ich sumę, różnicę, iloczyn i
+            //iloraz.
+            double a, b, suma, różnica, iloczyn, iloraz;
+            Console.Write("podaj a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("podaj b=");
+            b = Convert.ToDouble(Console.ReadLine());
+            suma = a + b;
+            różnica = a - b;
+            iloczyn = a * b;
+            iloraz = a / b;
+            Console.WriteLine("{0}+{1}={2}", a, b, suma);
+            Console.WriteLine("{0}-{1}={2}", a, b, różnica);
+            Console.WriteLine("{0}*{1}={2}", a, b, iloczyn);
+            Console.WriteLine("{0}/{1}={2}", a, b, iloraz);
+
+            Console.ReadKey(true); //pauza 
+           
+
+                
+
+        }
+    }
+}
---
+++ Zadanie7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby całkowite i wypisujący na ekranie iloraz tych liczb z
+            //uwzględnieniem części całkowitej i reszty z dzielenia.
+            int a, b;
+            Console.Write("Podaj a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj b=");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}/{1}={2}r{3}", a, b, a/b, a%b);
+            Console.ReadKey(true);
+
+        }
+    }
+}
