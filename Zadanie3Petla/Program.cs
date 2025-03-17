94911cda82d6d33be1a11fa60d7cf116a43355a3
KIE_Admin <KIE_Admin@WZR-C24-22>
2025-03-17 16:04:10 +0100

    koniec

---
+++ Zadanie3Petla/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3Petla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Napisać program wczytujący liczbę całkowitą n (n>2) i wypisujący na ekranie wartość sumy
+
+            int n, i, suma = 0;
+            Console.Write("Podaj n= ");
+            n = Convert.ToInt32( Console.ReadLine() );
+
+            for (i = 1; i <= n; i++)
+                suma += i;
+            Console.WriteLine("1+...+{0}={1}", n, suma );
+            Console.ReadKey(true);
+
+        }
+    }
+}
--- Zadsanie1/Program.cs
+++ Zadsanie1/Program.cs
@@ -9,7 +9,12 @@ namespace Zadsanie1
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {//Napisać program wypisujący na ekranie kolejne liczby całkowite od 11 do 33. Użyj pętli for.
+            int i;
+            for ( i = 11; i <= 33;  i++ ) 
+                Console.Write("{0}", i );
+            Console.ReadKey(true);
         }
+        
     }
 }
